5f5a0bd58dc9eb6066bed553a390f12777a09658
0sinach1 <osimachifeanyi@gmail.com>
2025-07-09 05:05:03 +0100

    The JavaScript file that does all that work

--- home.js
+++ home.js
@@ -1,6 +1,15 @@
 const toggle = document.getElementById('theme-toggle');
-const html = document.getElementById('html');
+const dataPortfolio = document.getElementById('data-portfolio');
+const devPortfolio = document.getElementById('dev-portfolio');
 
-toggle.addEventListener('change', function () {
+toggle.addEventListener('change', () => {
     document.body.classList.toggle('dark_mode');
+
+    if (toggle.checked) {
+        dataPortfolio.style.display = 'none';
+        devPortfolio.style.display = 'block';
+    } else {
+        dataPortfolio.style.display = 'block';
+        devPortfolio.style.display = 'none';
+    }
 });
